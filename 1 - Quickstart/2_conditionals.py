0f797ea3c8004243ea45876998c0990070b1576b
Franco Sarachu <fsarachu@gmail.com>
2016-10-21 19:28:54 -0300

    inline conditional expression

--- 1 - Quickstart/2_conditionals.py
+++ 1 - Quickstart/2_conditionals.py
@@ -5,3 +5,5 @@ if a < b:
     print('a ({}) is less than b ({})'.format(a, b))
 else:
     print('b ({}) is less or equal than a ({})'.format(a, b))
+
+print("foo" if a < b else "bar")  # Ternary operator-like conditional expression
