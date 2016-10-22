ea80d21dda21979929f8eac21b26c4a9e1194f85
Franco Sarachu <fsarachu@gmail.com>
2016-10-21 23:13:57 -0300

    Remove print()'s from function, print only prime numbers

--- 1 - Quickstart/4_functions.py
+++ 1 - Quickstart/4_functions.py
@@ -1,18 +1,16 @@
 def isprime(n):
-    if n == 1:
-        print("  1 is special.")
+    if n <= 1:
         return False
 
     for x in range(2, n):
         if n % x == 0:
-            print("  {} is divisible by {}".format(n, x))
             return False
     else:
         # Confusing for...else construct explained at:
         # http://python-notes.curiousefficiency.org/en/latest/python_concepts/break_else.html
-        print("* {} is a prime number".format(n))
         return True
 
 
 for i in range(20):
-    isprime(i)
+    if isprime(i):
+        print(i)
