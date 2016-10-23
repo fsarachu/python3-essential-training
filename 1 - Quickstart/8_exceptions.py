18a1623fcae12f1e08c8a9ed3587ba9831ccbaf5
Franco Sarachu <fsarachu@gmail.com>
2016-10-22 23:53:01 -0300

    Narrow exception type and print original error message

--- 1 - Quickstart/8_exceptions.py
+++ 1 - Quickstart/8_exceptions.py
@@ -2,7 +2,7 @@ try:
     fh = open("xlines.txt")
     for line in fh.readlines():
         print(line)
-except:
-    print("something went wrong while reading the file")
+except IOError as e:
+    print("something went wrong: {}".format(e))
 
 print("After badness")  # Execution continues after handled error
