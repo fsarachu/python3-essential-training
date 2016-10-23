7d59d1f85ca19c569607be0ce1e2871c2c9b86bb
Franco Sarachu <fsarachu@gmail.com>
2016-10-22 23:40:00 -0300

    Inheritance example

---
+++ 1 - Quickstart/7_oop2.py
@@ -0,0 +1,23 @@
+class Person:
+    def __init__(self, first, last):
+        self.firstname = first
+        self.lastname = last
+
+    def name(self):
+        return self.firstname + " " + self.lastname
+
+
+class Employee(Person):
+    def __init__(self, first, last, staffnum):
+        Person.__init__(self, first, last)
+        self.staffnumber = staffnum
+
+    def get_employee(self):
+        return self.name() + ", " + self.staffnumber
+
+
+x = Person("Marge", "Simpson")
+y = Employee("Homer", "Simpson", "1007")
+
+print(x.name())
+print(y.get_employee())
