6788cceed9bcd9de2e3b1761303a938337fbb531
Franco Sarachu <fsarachu@gmail.com>
2016-10-22 23:44:35 -0300

    use __string__

--- 1 - Quickstart/7_oop2.py
+++ 1 - Quickstart/7_oop2.py
@@ -1,23 +1,24 @@
 class Person:
-    def __init__(self, first, last):
+    def __init__(self, first, last, age):
         self.firstname = first
         self.lastname = last
+        self.age = age
 
-    def name(self):
-        return self.firstname + " " + self.lastname
+    def __str__(self):
+        return self.firstname + " " + self.lastname + ", " + str(self.age)
 
 
 class Employee(Person):
-    def __init__(self, first, last, staffnum):
-        Person.__init__(self, first, last)
+    def __init__(self, first, last, age, staffnum):
+        super().__init__(first, last, age)
         self.staffnumber = staffnum
 
-    def get_employee(self):
-        return self.name() + ", " + self.staffnumber
+    def __str__(self):
+        return super().__str__() + ", " + self.staffnumber
 
 
-x = Person("Marge", "Simpson")
-y = Employee("Homer", "Simpson", "1007")
+x = Person("Marge", "Simpson", 36)
+y = Employee("Homer", "Simpson", 28, "1007")
 
-print(x.name())
-print(y.get_employee())
+print(x)
+print(y)
