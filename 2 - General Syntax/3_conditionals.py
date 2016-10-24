23ab27a653cfe4d06288660a354ad6e259eaee67
Franco Sarachu <fsarachu@gmail.com>
2016-10-23 23:01:59 -0300

    Simple conditional

---
+++ 2 - General Syntax/3_conditionals.py
@@ -0,0 +1,9 @@
+# There are two types of conditionals in python:
+#   - Conditional Executions
+#   - Conditional Values
+
+if __name__ == '__main__':
+    a, b = 0, 1
+
+    if a < b:
+        print("a ({}) is less than b ({})".format(a, b))
