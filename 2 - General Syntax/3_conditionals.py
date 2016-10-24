e4aa9e9fff7dbda64e38e5225b7915beb7ab568e
Franco Sarachu <fsarachu@gmail.com>
2016-10-23 23:07:48 -0300

    Add conditional value example

--- 2 - General Syntax/3_conditionals.py
+++ 2 - General Syntax/3_conditionals.py
@@ -1,8 +1,10 @@
 # There are two types of conditionals in python:
-#   - Conditional Executions
-#   - Conditional Values
+#   - Conditional Execution
+#   - Conditional Value
 
 if __name__ == '__main__':
+
+    # Conditional Execution:
     a, b = 0, 1
 
     if a < b:
@@ -11,3 +13,9 @@ if __name__ == '__main__':
         print("a ({}) is not less than b ({})".format(a, b))
     else:
         print("a ({}) is equal to b ({})".format(a, b))
+
+    # Conditional Value:
+    a, b = 0, 1
+    s = "less than" if a < b else "not less than"
+
+    print("a ({}) is {} b ({})".format(a, s, b))
