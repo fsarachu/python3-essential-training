4e3efa972400fdee9098b6c56d964ccdf485f3a9
Franco Sarachu <fsarachu@gmail.com>
2016-10-24 20:55:01 -0300

    Check some black magic

--- 3 - Variables, Objects and Values/1_variables.py
+++ 3 - Variables, Objects and Values/1_variables.py
@@ -2,13 +2,20 @@
 # Every object has an id, a class type and a value
 
 a = 4
-print("id: {}".format(id(a)))
+print("id: {}".format(id(a)))  # Object id
 print("type: {}".format(type(a)))
 print("value: {}".format(a))
 
 print("---------------------------")
 
 a = "Hello"
-print("id: {}".format(id(a)))
+print("id: {}".format(id(a)))  # Now 'a' refers to another object
+print("type: {}".format(type(a)))
+print("value: {}".format(a))
+
+print("---------------------------")
+
+a = 4
+print("id: {}".format(id(a)))  # Same id as in the first assignment! :O
 print("type: {}".format(type(a)))
 print("value: {}".format(a))
