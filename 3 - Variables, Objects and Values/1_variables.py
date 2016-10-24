e4cd4595791cb33fc6df65f3650f9715ef6863fa
Franco Sarachu <fsarachu@gmail.com>
2016-10-24 20:49:20 -0300

    Variable basics in python

---
+++ 3 - Variables, Objects and Values/1_variables.py
@@ -0,0 +1,14 @@
+# In Python 3 EVERYTHING is an object
+# Every object has an id, a class type and a value
+
+a = 4
+print("id: {}".format(id(a)))
+print("type: {}".format(type(a)))
+print("value: {}".format(a))
+
+print("---------------------------")
+
+a = "Hello"
+print("id: {}".format(id(a)))
+print("type: {}".format(type(a)))
+print("value: {}".format(a))
