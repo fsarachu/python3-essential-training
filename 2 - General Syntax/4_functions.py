73f76d4d5307fef3a18ae5b361c23c5911f1da0f
Franco Sarachu <fsarachu@gmail.com>
2016-10-24 03:52:51 -0300

    Define a function with parameter

--- 2 - General Syntax/4_functions.py
+++ 2 - General Syntax/4_functions.py
@@ -1,9 +1,16 @@
 def main():
     func()
+    func_with_parameter(3)
 
 
 def func():
     print("This is a function!")
+
+
+def func_with_parameter(a):
+    for i in range(a, 10):
+        print(i, end=' ')
+    print('')
 
 
 if __name__ == '__main__':
