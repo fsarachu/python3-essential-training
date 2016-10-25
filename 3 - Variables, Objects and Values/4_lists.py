5b551df201761b73fd579246359d6999440ec185
Franco Sarachu <fsarachu@gmail.com>
2016-10-25 02:19:10 -0300

    Some list methods

--- 3 - Variables, Objects and Values/4_lists.py
+++ 3 - Variables, Objects and Values/4_lists.py
@@ -1,5 +1,24 @@
 # A list is mutable, but less efficient than a tuple
-x = [1, 2, 3]
-x.append(5)
-x.insert(3, 4)
-print(type(x), x)
+
+# Some list methods
+a = [66.25, 333, 333, 1, 1234.5]
+print("Initial list: {}".format(a))
+print("count(333): {}, count(66.25): {}, count('x'): {} ".format(a.count(333), a.count(66.25), a.count('x')))
+
+a.insert(2, -1)
+a.append(333)
+print("After insert(2, -1) and append(333): {}".format(a))
+
+print("index(333): {}".format(a.index(333)))
+
+a.remove(333)
+print("After remove(333): {}".format(a))
+
+a.reverse()
+print("After reverse(): {}".format(a))
+
+a.sort()
+print("After sort(): {}".format(a))
+
+print("pop(): {}".format(a.pop()))
+print("After pop(): {}".format(a))
