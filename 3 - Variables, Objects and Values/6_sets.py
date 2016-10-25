32ece8f76b35950948f1de1dde5b955dd65b3353
Franco Sarachu <fsarachu@gmail.com>
2016-10-25 03:36:24 -0300

    Add output separators

--- 3 - Variables, Objects and Values/6_sets.py
+++ 3 - Variables, Objects and Values/6_sets.py
@@ -5,13 +5,19 @@
 s = set()  # Note that "s = {}" would create an empty dictionary
 print("s: {}".format(s))
 
+print("----------------------------")
+
 # Duplicates are automatically removed
 animals = {'kangaroo', 'elephant', 'jaguar', 'elephant', 'lion'}
 print(animals)
 
+print("----------------------------")
+
 # Membership testing
 print("'kangaroo' in animals: {}".format('kangaroo' in animals))
 print("'dog' in animals: {}".format('dog' in animals))
+
+print("----------------------------")
 
 # Set operations
 a = set('abracadabra')
@@ -22,3 +28,4 @@ print("a - b: {}".format(a - b))  # letters in a but not in b
 print("a | b: {}".format(a | b))  # letters in either a or b
 print("a & b: {}".format(a & b))  # letters in both a and b
 print("a ^ b: {}".format(a ^ b))  # letters in a or b but not both
+
