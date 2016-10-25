46712f6fb203e9fa283d9dc347a674bf1eedb76e
Franco Sarachu <fsarachu@gmail.com>
2016-10-25 02:26:51 -0300

    List as stack

--- 3 - Variables, Objects and Values/4_lists.py
+++ 3 - Variables, Objects and Values/4_lists.py
@@ -1,24 +1,42 @@
 # A list is mutable, but less efficient than a tuple
 
-# Some list methods
+print("\n--- Some List Methods ---")
+
 a = [66.25, 333, 333, 1, 1234.5]
-print("Initial list: {}".format(a))
-print("count(333): {}, count(66.25): {}, count('x'): {} ".format(a.count(333), a.count(66.25), a.count('x')))
+print("a: {}".format(a))
+print("a.count(333): {}, a.count(66.25): {}, a.count('x'): {} ".format(a.count(333), a.count(66.25), a.count('x')))
 
 a.insert(2, -1)
 a.append(333)
-print("After insert(2, -1) and append(333): {}".format(a))
+print("a.insert(2, -1) and a.append(333): {}".format(a))
 
-print("index(333): {}".format(a.index(333)))
+print("a.index(333): {}".format(a.index(333)))
 
 a.remove(333)
-print("After remove(333): {}".format(a))
+print("a.remove(333): {}".format(a))
 
 a.reverse()
-print("After reverse(): {}".format(a))
+print("a.reverse(): {}".format(a))
 
 a.sort()
-print("After sort(): {}".format(a))
+print("a.sort(): {}".format(a))
 
-print("pop(): {}".format(a.pop()))
-print("After pop(): {}".format(a))
+print("a.pop(): {}".format(a.pop()))
+print("After a.pop(): {}".format(a))
+
+print("\n--- Lists as Stacks ---")
+
+stack = [1, 2, 3]
+print("stack: {}".format(stack))
+
+stack.append(4)
+print("stack.append(4): {}".format(stack))
+
+stack.append(5)
+print("stack.append(5): {}".format(stack))
+
+print("stack.pop(): {}".format(stack.pop()))
+print("stack.pop(): {}".format(stack.pop()))
+print("stack.pop(): {}".format(stack.pop()))
+
+print("stack: {}".format(stack))
