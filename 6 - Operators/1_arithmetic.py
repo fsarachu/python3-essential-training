875762950dd8ff9a4b495e7e231d04915f70f632
Franco Sarachu <fsarachu@gmail.com>
2016-10-25 20:14:40 -0300

    Arithmetic operators

---
+++ 6 - Operators/1_arithmetic.py
@@ -0,0 +1,9 @@
+# Arithmetic operators
+print("5 + 3 = {}".format(5 + 3))
+print("5 * 3 = {}".format(5 * 3))
+print("5 - 3 = {}".format(5 - 3))
+print("5 / 3 = {}".format(5 / 3))
+print("5 // 3 = {}".format(5 // 3))
+print("5 % 3 = {}".format(5 % 3))
+print("divmod(5, 3) = {}".format(divmod(5, 3)))
+
