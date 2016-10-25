d028bc94dc65584b0b466ac742b610b2dd66b8e1
Franco Sarachu <fsarachu@gmail.com>
2016-10-25 16:21:11 -0300

    else clause after loop

--- 5 - Loops/4_loop_control.py
+++ 5 - Loops/4_loop_control.py
@@ -1,21 +1,39 @@
 s = 'this is a string'
 
+print("\n--- Normal for loop ---")
+
 for c in s:
     print(c, end=' ')
 
-print("\n--------------------")
+print("\n\n--- Continue if a 's' is found ---")
 
 for c in s:
     if c == 's':
         continue
     print(c, end=' ')
 
-print("\n--------------------")
+print("\n\n--- Break if a 's' is found ---")
 
 for c in s:
     if c == 's':
         break
     print(c, end=' ')
 
-print("\n--------------------")
+print("\n\n--- Break if a 's' is found and print message if the loop has completed ---")
+
+for c in s:
+    if c == 's':
+        break
+    print(c, end=' ')
+else:
+    print('\nLoop completed successfully')
+
+print("\n\n--- Break if a 'w' is found and print message if the loop has completed ---")
+
+for c in s:
+    if c == 'w':
+        break
+    print(c, end=' ')
+else:
+    print('\nLoop completed successfully')
 
