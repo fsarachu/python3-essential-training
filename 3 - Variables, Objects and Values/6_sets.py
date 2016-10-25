7d4f3c211e64cbe81b9a37a86d4a2f8d63a7cf22
Franco Sarachu <fsarachu@gmail.com>
2016-10-25 03:33:53 -0300

    Sets

---
+++ 3 - Variables, Objects and Values/6_sets.py
@@ -0,0 +1,20 @@
+# A set is an unordered collection with no duplicate elements.
+# Basic uses include membership testing and eliminating duplicate entries.
+
+# Empty set
+s = set()  # Note that "s = {}" would create an empty dictionary
+print("s: {}".format(s))
+
+# Duplicates are automatically removed
+animals = {'kangaroo', 'elephant', 'jaguar', 'elephant', 'lion'}
+print(animals)
+
+# Set operations
+a = set('abracadabra')
+b = set('alacazam')
+
+print("a: {}".format(a))  # unique letters in a
+print("a - b: {}".format(a - b))  # letters in a but not in b
+print("a | b: {}".format(a | b))  # letters in either a or b
+print("a & b: {}".format(a & b))  # letters in both a and b
+print("a ^ b: {}".format(a ^ b))  # letters in a or b but not both
