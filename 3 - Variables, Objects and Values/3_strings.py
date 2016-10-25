0b32224392ee3dd6c4abca97c798f7bca5ebf8c3
Franco Sarachu <fsarachu@gmail.com>
2016-10-25 01:27:28 -0300

    Add triple quoted string

--- 3 - Variables, Objects and Values/3_strings.py
+++ 3 - Variables, Objects and Values/3_strings.py
@@ -10,3 +10,11 @@ print(s)
 n = 42
 s = "This number is inserted with string formatting: {}".format(n)
 print(s)
+
+# First backslash is escaping the newline in the code
+s = '''\
+This is a triple
+quoted string that can
+extend multiple lines
+'''
+print(s)
