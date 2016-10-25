0f8a3970915c5e1a3c7d7e600c5ec8a866ce5c72
Franco Sarachu <fsarachu@gmail.com>
2016-10-25 01:29:40 -0300

    Add separators to the output

--- 3 - Variables, Objects and Values/3_strings.py
+++ 3 - Variables, Objects and Values/3_strings.py
@@ -1,20 +1,30 @@
 s = 'This is a string!'  # Single and double quotes are the same
 print(s)
 
+print("----------------------")
+
 s = 'This is a string with a \nescape character!'
 print(s)
 
+print("----------------------")
+
 s = r"This is a Raw\n string"  # Raw string are not preprocessed, no escape characters are interpreted
 print(s)
+
+print("----------------------")
 
 n = 42
 s = "This number is inserted with string formatting: {}".format(n)
 print(s)
 
-# First backslash is escaping the newline in the code
+print("----------------------")
+
+# Backslashes are escaping the newlines in the code
 s = '''\
 This is a triple
 quoted string that can
-extend multiple lines
+extend multiple lines\
 '''
 print(s)
+
+print("----------------------")
