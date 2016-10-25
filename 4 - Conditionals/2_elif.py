80be4ed6c0542ad4619125ea3f503bf19e17752f
Franco Sarachu <fsarachu@gmail.com>
2016-10-25 13:58:37 -0300

    elif

---
+++ 4 - Conditionals/2_elif.py
@@ -0,0 +1,10 @@
+s = 'two'
+
+if s == 'one':
+    print('s is one')
+elif s == 'two':
+    print('s is two')
+elif s == 'three':
+    print('s is three')
+else:
+    print('s is something else: {}'.format(s))
