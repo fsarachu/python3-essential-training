a9eae34f36b4eff6977daa4e134a7f9b22830815
Franco Sarachu <fsarachu@gmail.com>
2016-10-25 20:40:09 -0300

    Bitwise operators

--- 6 - Operators/2_bitwise.py
+++ 6 - Operators/2_bitwise.py
@@ -2,5 +2,27 @@ def b(n):
     print("{:08b}".format(n))  # Print binary representation of 'n'
 
 
-n = 5
-b(n)
+x, y = 0x55, 0xaa
+
+print("x: ", end='')
+b(x)
+print("y: ", end='')
+b(y)
+
+print("x | y: ", end='')
+b(x | y)
+
+print("x & y: ", end='')
+b(x & y)
+
+print("x ^ y: ", end='')
+b(x ^ y)
+
+print("x ^ x: ", end='')
+b(x ^ x)
+
+print("x << 2: ", end='')
+b(x << 2)
+
+print("x >> 5: ", end='')
+b(x >> 5)
