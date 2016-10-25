3ae4b5f2d4faba3fe3b49f1692b06e18113384cc
Franco Sarachu <fsarachu@gmail.com>
2016-10-24 21:04:17 -0300

    Remove object id print

--- 3 - Variables, Objects and Values/2_numbers.py
+++ 3 - Variables, Objects and Values/2_numbers.py
@@ -1,35 +1,30 @@
 # In Python there ara only integers and floats
 
 num = 23
-print("id: {}".format(id(num)))
-print("type: {}".format(type(num)))
 print("value: {}".format(num))
+print("type: {}".format(type(num)))
 
 print("---------------------------")
 
 num = 23 / 4  # Normal division (float result)
-print("id: {}".format(id(num)))
-print("type: {}".format(type(num)))
 print("value: {}".format(num))
+print("type: {}".format(type(num)))
 
 print("---------------------------")
 
 num = 23 // 4  # Integer division
-print("id: {}".format(id(num)))
-print("type: {}".format(type(num)))
 print("value: {}".format(num))
+print("type: {}".format(type(num)))
 
 print("---------------------------")
 
 num = int(23 / 4)  # Explicit cast to int
-print("id: {}".format(id(num)))
-print("type: {}".format(type(num)))
 print("value: {}".format(num))
+print("type: {}".format(type(num)))
 
 print("---------------------------")
 
 num = float(23)  # Explicit cast to float
-print("id: {}".format(id(num)))
-print("type: {}".format(type(num)))
 print("value: {}".format(num))
+print("type: {}".format(type(num)))
 
