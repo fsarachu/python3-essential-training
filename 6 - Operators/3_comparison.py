5793960792a36a4d1b5f24e84e14f6bcbd8a5de1
Franco Sarachu <fsarachu@gmail.com>
2016-10-25 20:57:59 -0300

    Comparison operators

---
+++ 6 - Operators/3_comparison.py
@@ -0,0 +1,22 @@
+print("5 < 6: {}".format(5 < 6))
+print("5 > 6: {}".format(5 > 6))
+print("5 == 6: {}".format(5 == 6))
+print("5 <= 6: {}".format(5 <= 6))
+print("5 >= 6: {}".format(5 >= 6))
+print("5 != 6: {}".format(5 >= 6))
+
+print("--------------------------")
+
+x, y = 5, 6
+print("x, y = 5, 6")
+
+print("id(x): {}".format(id(x)))
+print("id(y): {}".format(id(y)))
+
+print("x is y: {}".format(x is y))
+print("x is not y: {}".format(x is not y))
+
+y = 5
+print("y = 5")
+
+print("x is y: {}".format(x is y))
