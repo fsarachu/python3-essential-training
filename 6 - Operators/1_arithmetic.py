e9c4d32022f235fe0b6507cba9c1293b2854b782
Franco Sarachu <fsarachu@gmail.com>
2016-10-25 20:20:10 -0300

    Assignment operators

--- 6 - Operators/1_arithmetic.py
+++ 6 - Operators/1_arithmetic.py
@@ -1,4 +1,4 @@
-# Arithmetic operators
+print("\n--- Arithmetic operators ---")
 print("5 + 3 = {}".format(5 + 3))
 print("5 * 3 = {}".format(5 * 3))
 print("5 - 3 = {}".format(5 - 3))
@@ -7,3 +7,16 @@ print("5 // 3 = {}".format(5 // 3))
 print("5 % 3 = {}".format(5 % 3))
 print("divmod(5, 3) = {}".format(divmod(5, 3)))
 
+print("\n--- Assignment operators ---")
+n = 10
+print("n = {}".format(n))
+n += 3
+print("n += 3 -> n = {}".format(n))
+n *= 3
+print("n *= 3 -> n = {}".format(n))
+n -= 3
+print("n -= 3 -> n = {}".format(n))
+n /= 3
+print("n /= 3 -> n = {}".format(n))
+n //= 3
+print("n //= 3 -> n = {}".format(n))
