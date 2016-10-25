365fa3e3161a1f4816e09c95d7c069cb665eea76
Franco Sarachu <fsarachu@gmail.com>
2016-10-25 14:18:08 -0300

    Conditional expressions

---
+++ 4 - Conditionals/4_conditional_expressions.py
@@ -0,0 +1,10 @@
+a, b = 0, 1
+
+# With conditional execution
+if a < b:
+    print('this is true')
+else:
+    print('this is not true')
+
+# With conditional expression
+print('this is true' if a < b else 'this is not true')
