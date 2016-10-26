2a2b391edd95a9cc9b223e258469048cc93598dd
Franco Sarachu <fsarachu@gmail.com>
2016-10-26 13:52:34 -0300

    Print only replaced lines

--- 7 - Regex/2_search_and_replace.py
+++ 7 - Regex/2_search_and_replace.py
@@ -3,4 +3,6 @@ import re
 fh = open('raven.txt')
 
 for line in fh.readlines():
-    print(re.sub('((l|L)en|(N|n)everm)ore', '*****', line), end='')
+    match = re.search('((l|L)en|(N|n)everm)ore', line)
+    if match:
+        print(line.replace(match.group(), '***'), end='')
