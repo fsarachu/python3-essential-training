131177b1220ccad6587a0ee73de91852f5fcdb60
Franco Sarachu <fsarachu@gmail.com>
2016-10-26 13:46:14 -0300

    Use match object

--- 7 - Regex/1_regexsearch.py
+++ 7 - Regex/1_regexsearch.py
@@ -3,5 +3,6 @@ import re
 fh = open('raven.txt')
 
 for line in fh.readlines():
-    if re.search('((l|L)en|(N|n)everm)ore', line):
-        print(line, end='')
+    match = re.search('((l|L)en|(N|n)everm)ore', line)
+    if match:
+        print(match.group())
