27b519dc3be64b945b8ec86949e8f8a8a97ccb7f
Franco Sarachu <fsarachu@gmail.com>
2016-10-27 17:33:28 -0300

    InclusiveRange class outline

---
+++ 10 - Classes/5_generator.py
@@ -0,0 +1,17 @@
+class InclusiveRange:
+    def __init__(self):
+        pass
+
+    def __iter__(self):
+        pass
+
+
+def main():
+    o = range(25)
+
+    for i in o:
+        print(i, end=' ')
+
+
+if __name__ == '__main__':
+    main()
