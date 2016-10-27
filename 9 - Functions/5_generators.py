222c489606c81eb2ab0897340651455b092207df
Franco Sarachu <fsarachu@gmail.com>
2016-10-26 23:48:28 -0300

    Inclusive range generator

---
+++ 9 - Functions/5_generators.py
@@ -0,0 +1,16 @@
+def main():
+    for n in inclusive_range(3, 9):
+        print(n, end=' ')
+
+
+def inclusive_range(start, stop, steps=1):
+    i = start
+
+    while i <= stop:
+        yield i
+        print('after yield')
+        i += steps
+
+
+if __name__ == '__main__':
+    main()
