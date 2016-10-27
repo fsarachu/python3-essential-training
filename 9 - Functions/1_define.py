a57690f47ea943760115cdfe154377f36bd0a9b8
Franco Sarachu <fsarachu@gmail.com>
2016-10-26 23:08:20 -0300

    Default value using None

--- 9 - Functions/1_define.py
+++ 9 - Functions/1_define.py
@@ -2,7 +2,10 @@ def main():
     some_function(c=10)
 
 
-def some_function(a=0, b=0, c=0):
+def some_function(a=0, b=None, c=0):
+    if b is None:
+        b = 0
+
     print('This is a function')
     print('a: {}, b: {}, c: {}'.format(a, b, c))
 
