e118f9a2f1a2149d532efb0a930948e04f23425c
Franco Sarachu <fsarachu@gmail.com>
2016-10-27 17:40:40 -0300

    Define InclusiveRange constructor

--- 10 - Classes/5_generator.py
+++ 10 - Classes/5_generator.py
@@ -1,6 +1,23 @@
 class InclusiveRange:
-    def __init__(self):
-        pass
+    def __init__(self, *args):
+        arg_count = len(args)
+
+        if arg_count < 1:
+            raise TypeError('At least 1 argument is expected')
+        elif arg_count == 1:
+            self.start = 0
+            self.stop = args[0]
+            self.steps = 1
+        elif arg_count == 2:
+            self.start = args[0]
+            self.stop = args[1]
+            self.steps = 1
+        elif arg_count == 3:
+            self.start = args[0]
+            self.stop = args[1]
+            self.steps = args[2]
+        else:
+            raise TypeError('Too many arguments, at most 3 are spected, got {}'.format(arg_count))
 
     def __iter__(self):
         pass
