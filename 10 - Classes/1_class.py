a43620f6f4de5cb621fffda380014ddf02f82059
Franco Sarachu <fsarachu@gmail.com>
2016-10-27 00:44:22 -0300

    Simple class definition

---
+++ 10 - Classes/1_class.py
@@ -0,0 +1,13 @@
+class Duck:
+    def quack(self):
+        print('Quaaaackkk!')
+
+    def walk(self):
+        print('I\'m a duck walking!')
+
+
+if __name__ == '__main__':
+    lilly = Duck()
+
+    lilly.quack()
+    lilly.walk()
