2233cd289439980e4091b0b7437933297d7d4345
Franco Sarachu <fsarachu@gmail.com>
2016-10-27 18:04:36 -0300

    Define property accessor methods with decorators

--- 10 - Classes/6_decorators.py
+++ 10 - Classes/6_decorators.py
@@ -1,16 +1,32 @@
 class Duck:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, **kwargs):
+        self.properties = dict()
+        self.properties['name'] = kwargs.get('name', 'Anonymous Duck')
 
     def quack(self):
-        print('{} quaaaackkks!'.format(self.name))
+        print('{} quaaaackkks!'.format(self.properties['name']))
 
     def walk(self):
-        print('{} is walking!'.format(self.name))
+        print('{} is walking!'.format(self.properties['name']))
+
+    @property
+    def name(self):
+        return self.properties.get('name', None)
+
+    @name.setter
+    def name(self, n):
+        self.properties['name'] = n
+
+    @name.deleter
+    def name(self):
+        del self.properties['name']
 
 
 if __name__ == '__main__':
-    lilly = Duck('Lilly')
+    lilly = Duck()
 
-    lilly.quack()
-    lilly.walk()
+    print(lilly.name)
+
+    lilly.name = 'Lilly'
+
+    print(lilly.name)
