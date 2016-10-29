1f4abe6501970a11f7b9eb4aa45c07bb22252d40
Franco Sarachu <fsarachu@gmail.com>
2016-10-28 22:32:07 -0300

    Some common string methods

---
+++ 11 - String Methods/2_string_methods.py
@@ -0,0 +1,15 @@
+s = 'This Is a String!'
+
+print('s: {}'.format(s))
+print('s.lower(): {}'.format(s.lower()))
+print('s.upper(): {}'.format(s.upper()))
+print('s.capitalize(): {}'.format(s.capitalize()))
+print('s.swapcase(): {}'.format(s.swapcase()))
+print('s.find(\'String\'): {}'.format(s.find('String')))
+print('s.replace(\'This\', \'That\'): {}'.format(s.replace('This', 'That')))
+print('\'    This is a string      \'.strip(): {}'.format('    This is a string      '.strip()))
+print('\'    This is a string      \'.rstrip(): {}'.format('    This is a string      '.rstrip()))
+print('\'This is a string\'.isalnum(): {}'.format('This is a string'.isalnum()))
+print('\'Thisisastring\'.isalnum(): {}'.format('Thisisastring'.isalnum()))
+print('\'Thisisastring\'.isalpha(): {}'.format('Thisisastring'.isalpha()))
+print('\'12345\'.isdigit(): {}'.format('12345'.isdigit()))
