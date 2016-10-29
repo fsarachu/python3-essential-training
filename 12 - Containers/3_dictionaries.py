e9cfe9319c966ac72d04997ef36652a60f25deb4
Franco Sarachu <fsarachu@gmail.com>
2016-10-29 01:46:38 -0300

    Renamed variable

--- 12 - Containers/3_dictionaries.py
+++ 12 - Containers/3_dictionaries.py
@@ -1,7 +1,8 @@
-d1 = {'one': 1, 'two': 2, 'three': 3}
+d = {'one': 1, 'two': 2, 'three': 3}
 
-print('d1 : {}'.format(d1))
+print('d : {}'.format(d))
 
-d2 = dict(one=1, two=2, three=3)
+d = dict(one=1, two=2, three=3)
 
-print('d2 : {}'.format(d2))
+print('d : {}'.format(d))
+
