7bf1466a75af59a35a1e53cb6a8c103943b70e53
Franco Sarachu <fsarachu@gmail.com>
2016-10-28 23:06:09 -0300

    Formatting with dictionary

--- 11 - String Methods/3_string_format.py
+++ 11 - String Methods/3_string_format.py
@@ -3,3 +3,9 @@ a, b = 5, 42
 s1 = 'a is {0}, b is {1}, again a is {0}'
 
 print(s1.format(a, b))
+
+s2 = 'first is {bob}, second is {fred}'
+
+names = dict(bob='bobby', fred='freddy')
+
+print(s2.format(**names))
