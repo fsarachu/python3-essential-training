1e82bec6992ee24e35e8a7326649d7e9fd3d73cf
Franco Sarachu <fsarachu@gmail.com>
2016-10-28 23:31:50 -0300

    Simple split and join

---
+++ 11 - String Methods/4_split_join.py
@@ -0,0 +1,11 @@
+s = 'This is a string of words'
+
+print('s: {}'.format(s))
+
+splitted = s.split()
+
+print('splited = s.split(): {}'.format(splitted))
+
+joined = '_'.join(splitted)
+
+print('joined = \'_\'.join(splitted): {}'.format(joined))
