806c9702cfdee501584dc2ef086cd99482c5eda6
Franco Sarachu <fsarachu@gmail.com>
2016-10-30 16:14:46 -0300

    Insert some test values

--- 14 - Databases/1_creating.py
+++ 14 - Databases/1_creating.py
@@ -4,3 +4,10 @@ db = _sqlite3.connect('test.db')
 
 db.execute('DROP TABLE IF EXISTS test')
 db.execute('CREATE TABLE test(t1 TEXT, i1 INT)')
+
+values = dict(one=1, two=2, three=3, four=4)
+
+for k in values:
+    db.execute('INSERT INTO test (t1, i1) VALUES (?, ?)', (k, values[k]))
+
+db.commit()
