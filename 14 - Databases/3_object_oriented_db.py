7208b74fe91a8d1d06a47822041fa658b145daac
Franco Sarachu <fsarachu@gmail.com>
2016-10-31 12:35:43 -0300

    Define sql method and run main

--- 14 - Databases/3_object_oriented_db.py
+++ 14 - Databases/3_object_oriented_db.py
@@ -4,7 +4,7 @@ import _sqlite3
 class Database:
     def __init__(self, db_name):
         self._properties = dict()
-        self._properties['connection'] = _sqlite3.connect('db_name')
+        self._properties['connection'] = _sqlite3.connect(db_name)
         self._properties['connection'].execute('DROP TABLE IF EXISTS test')
         self._properties['connection'].execute('CREATE TABLE test(t1 TEXT, i1 INT)')
 
@@ -12,9 +12,17 @@ class Database:
     def connection(self):
         return self._properties['connection']
 
+    def sql(self, sql_string, commit=True):
+        result = self.connection.execute(sql_string)
+        if commit:
+            self.connection.commit()
+
+        return result
+
 
 def main():
     db = Database('oop.db')
+    db.sql('INSERT INTO test (t1, i1) VALUES ("one", 1)')
 
 
 if __name__ == '__main__':
