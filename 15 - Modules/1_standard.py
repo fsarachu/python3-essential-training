02c5ce22a243fbfd4cfbf15166eb2c8b4f2a7ba3
Franco Sarachu <fsarachu@gmail.com>
2016-11-01 16:33:15 -0300

    Add print statement descriptions

--- 15 - Modules/1_standard.py
+++ 15 - Modules/1_standard.py
@@ -3,12 +3,13 @@ import sys
 
 def main():
     print('Python version: {}.{}.{}'.format(*sys.version_info))
-    print(sys.platform)
+    print('Platform: {}'.format(sys.platform))
 
     import os
 
-    print(os.name)
-    print(os.getenv('PATH'))
+    print('os name: {}'.format(os.name))
+    print('PATH variable: {}'.format(os.getenv('PATH')))
+    print('cwd: {}'.format(os.getcwd()))
 
 
 if __name__ == '__main__':
