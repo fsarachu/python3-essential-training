f9baa6f7b0d1ddf5ccbc4e9da468fda06a0ea33e
Franco Sarachu <fsarachu@gmail.com>
2016-11-01 14:39:31 -0300

    Use os module and define main function

--- 15 - Modules/1_standard.py
+++ 15 - Modules/1_standard.py
@@ -1,4 +1,16 @@
 import sys
 
-print('Python version: {}.{}.{}'.format(*sys.version_info))
-print(sys.platform)
+
+def main():
+    print('Python version: {}.{}.{}'.format(*sys.version_info))
+    print(sys.platform)
+
+    import os
+
+    print(os.name)
+    print(os.getenv('PATH'))
+
+
+if __name__ = '__main__':
+    main()
+
